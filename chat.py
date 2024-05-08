5f546f024d4e9ac3c309ba32db643ff81239270c
Sandesh Tidake0408 <sandeshtidake37@gmail.com>
2024-05-09 00:16:27 +0530

    Chat file

--- chat.py
+++ chat.py
@@ -29,3 +29,18 @@ def get_response(user_input):
 
 # Test the bot
 print(get_response("hello"))
+
+
+# pairs = {
+#     "Hello": "Hi how are you",
+#     "how are you": "Good, How can i help you?",
+#     "Bye": "Bye, Have a good day"
+# }
+
+# print("How can i help you ?")
+# while (True):
+#     x = input()
+#     if x in pairs.keys():
+#         print(pairs[x])
+#     else:
+#         print("incorrect Input")
